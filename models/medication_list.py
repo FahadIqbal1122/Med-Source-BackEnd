8b1e3a8db887e23e17c11e69be5cc152adc00cbf
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2024-07-04 21:33:20 +0300

    Run alchemy commands fixed issues

--- models/medication_list.py
+++ models/medication_list.py
@@ -5,18 +5,18 @@ class MedicationList(db.Model):
     __tablename__ = 'medication_list'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    # product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
     total_amount = db.Column(db.Float, nullable=False)
 
     def __init__(self, user_id, product_id, total_amount):
         self.user_id = user_id
-        self.product_id = product_id
+        # self.product_id = product_id
         self.total_amount = total_amount
 
     def json(self):
         return {"id": self.id,
             "user_id": self.user_id,
-            "product_id": self.product_id,
+            # "product_id": self.product_id,
             "total_amount": self.total_amount}
     
     def create(self):
