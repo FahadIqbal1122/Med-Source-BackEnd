280ea9aab613308acfa18ce7b8f1ef0b773b9f69
OsamaMohammad61 <osamamohammad343@gmail.com>
2024-07-04 14:34:40 +0300

    Made the user models and its function in the DB

---
+++ resources/user.py
@@ -0,0 +1,16 @@
+from flask_restful import Resource
+from flask import request
+from models.user import User
+from models.db import db
+
+class Users(Resource):
+    def get(self):
+        data = User.find_all()
+        results = [u.json() for u in data]
+        return results
+    
+    def post(self):
+        data = request.get_json()
+        user = User(**data)
+        user.create()
+        return user.json(), 201
